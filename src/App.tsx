98c87ee35ee9c7176d7c51af1aa5c8a800dd9d5a
navneetprajapati <navneet.p@internshala.com>
2025-04-17 12:00:01 +0530

    gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <Router>
       <Layout>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Home />} />
         </Routes>
       </Layout>
     </Router>
